9a6ae4dd9a8a15cdf812acd29469d0daa30b8ea6
Pasi Huttunen <pahuttun@hotmail.com>
2014-03-30 20:19:00 +0300

    Merged index.html with node.js

--- public/javascripts/simple.js
+++ public/javascripts/simple.js
@@ -5,8 +5,10 @@
         {
             // create a text object
             var textSprite = new TextSprite("ASS POO HJS", '22px Arial', 'blue', 'center');
-            textSprite.setMaxWidth(400);
+            textSprite.setMaxWidth(300);
             var textObject = new SceneObject(textSprite);
+            //textObject.setSpriteOffset(1, {x:-300, y: -300});
             wade.addSceneObject(textObject);
+            textObject.moveTo(-200, -200, 100);
         }
     };
